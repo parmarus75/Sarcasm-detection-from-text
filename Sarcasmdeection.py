5b344eca218c7048591d1f88f41d4caef2705092
parmarus75 <parmarsiddharth75@gmail.com>
2019-07-22 17:58:31 -0400

    Update Sarcasmdeection.py

--- Sarcasmdeection.py
+++ Sarcasmdeection.py
@@ -7,42 +7,51 @@ Created on Wed May 29 12:02:36 2019
 
 
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import spacy 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.metrics import confusion_matrix, classification_report, roc_curve, auc
-seed=100
-pip install spacy && python -m spacy download en
-
-
-#Importing dataset
-
-dataset = pd.read_json('Sarcasm_Headlines_Dataset.json', lines=True)
-dataset.head()
-
-#creating column source by using lambda function
+pd.set_option('display.max_colwidth', -1)
+path = '../input/'
+sar_acc = pd.read_json(path+ 'Sarcasm_Headlines_Dataset.json',lines=True)
 import re
-dataset['source'] = dataset['article_link'].apply(lambda x: re.findall(r'\w+', x)[2])
-dataset.head()
+sar_acc['source'] = sar_acc['article_link'].apply(lambda x: re.findall(r'\w+', x)[2])
+sar_acc.head()
 
-#data preprocessing
-dataset['num_words'] = dataset['headline'].apply(lambda x: len(str(x).split()))
-#dataset1=dataset['headline'].apply(lambda x: len(str(x)))
+# Statisics of the dataset
 
-print('Maximum number of word: ', dataset['num_words'].max())
-print('\nSentence:\n',dataset[dataset['num_words'] == 39]['headline'].values)
-text = dataset[dataset['num_words'] == 39]['headline'].values
+import numpy as np
+import plotly.plotly as py
+import plotly.graph_objs as go
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+init_notebook_mode(connected=True)
+
+sar_acc_tar = sar_acc['is_sarcastic'].value_counts()
+labels = ['Acclaim', 'Sarcastic']
+sizes = (np.array((sar_acc_tar / sar_acc_tar.sum())*100))
+colors = ['#58D68D', '#9B59B6']
+
+trace = go.Pie(labels=labels, values=sizes, opacity = 0.8, hoverinfo='label+percent',
+               marker=dict(colors=colors, line=dict(color='#FFFFFF', width=2)))
+layout = go.Layout(
+    title='Sarcastic Vs Acclaim'
+)
+data = [trace]
+fig = go.Figure(data=data, layout=layout)
+iplot(fig, filename="Sa_Ac")
 
 
-f = open('music.txt', 'r')
-sent = [word.lower().split() for word in f]
+# Frequent words in headline
+all_words = sar_acc['headline'].str.split(expand=True).unstack().value_counts()
+data = [go.Bar(
+            x = all_words.index.values[2:50],
+            y = all_words.values[2:50],
+            marker= dict(colorscale='Viridis',
+                         color = all_words.values[2:100]
+                        ),
+            text='Word counts'
+    )]
 
-token = [text.split() for word in text]
+layout = go.Layout(
+    title='Frequent Occuring word (unclean) in Headlines'
+)
 
+fig = go.Figure(data=data, layout=layout)
 
-
-
+iplot(fig, filename='basic-bar')

0aebcd24915e6a0fada7f78f9cd1e8bd69ad5a6e
parmarus75 <parmarsiddharth75@gmail.com>
2019-07-22 18:06:27 -0400

    Update Sarcasmdeection.py

--- Sarcasmdeection.py
+++ Sarcasmdeection.py
@@ -208,10 +208,238 @@ print("\nThe vectorized array looks like:\n {}".format(sentence_transform.toarra
 
 
 
+# Converting all sarcasm keywords to single list after lemmatization
+from matplotlib import pyplot as plt
+%matplotlib inline
+sar_list_wd = list(itertools.chain(*sar_wost_lem))
+from wordcloud import WordCloud
+sar_cloud = WordCloud(background_color='black', width=20000,height=10000).\
+                generate(" ".join(sar_list_wd))
+plt.imshow(sar_cloud)
+plt.axis('off')
+plt.show()
+
+
+# WordCLoud
+acc_list_wd = list(itertools.chain(*acc_wost_lem))
+acc_cloud = WordCloud(background_color='black', width=20000,height=10000).\
+                generate(" ".join(acc_list_wd))
+plt.imshow(acc_cloud)
+plt.axis('off')
+plt.show()
 
 
 
 
+
+sar_wost_lem_df = pd.DataFrame({'sarcasm':sar_wost_lem})
+acc_wost_lem_df = pd.DataFrame({'acclaim':acc_wost_lem})
+
+## custom function for ngram generation ##
+def generate_ngrams(text, n_gram=1):
+    ngrams = zip(*[text[i:] for i in range(n_gram)])
+    return [" ".join(ngram) for ngram in ngrams]
+
+## custom function for horizontal bar chart ##
+def horizontal_bar_chart(df, color):
+    trace = go.Bar(
+        y=df["word"].values[::-1],
+        x=df["wordcount"].values[::-1],
+        showlegend=False,
+        orientation = 'h',
+        marker=dict(
+            color=color,
+        ),
+    )
+    return trace
+
+#Plotting the Bigram plot
+from collections import defaultdict
+freq_dict = defaultdict(int)
+for sent in sar_wost_lem_df["sarcasm"]:
+    for word in generate_ngrams(sent,2):
+        freq_dict[word] += 1
+fd_sorted = pd.DataFrame(sorted(freq_dict.items(), key=lambda x: x[1])[::-1])
+fd_sorted.columns = ["word", "wordcount"]
+sar_2 = horizontal_bar_chart(fd_sorted.head(50), '#9B59B6')
+
+
+freq_dict = defaultdict(int)
+for sent in acc_wost_lem_df["acclaim"]:
+    for word in generate_ngrams(sent,2):
+        freq_dict[word] += 1
+fd_sorted = pd.DataFrame(sorted(freq_dict.items(), key=lambda x: x[1])[::-1])
+fd_sorted.columns = ["word", "wordcount"]
+acc_2 = horizontal_bar_chart(fd_sorted.head(50), '#58D68D')
+
+# Creating two subplots
+fig = tools.make_subplots(rows=1, cols=2, vertical_spacing=0.04,horizontal_spacing=0.15,
+                          subplot_titles=["Frequent bigrams of Sarcasm Headlines", 
+                                          "Frequent bigrams of Acclaim Headlines"])
+fig.append_trace(sar_2, 1, 1)
+fig.append_trace(acc_2, 1, 2)
+fig['layout'].update(height=1200, width=900, paper_bgcolor='rgb(233,233,233)', title="Bigram Plots Sarcasm Vs Acclaim after removing Stopwords")
+iplot(fig, filename='word-plots')
+
+
+
+
+#Plotting the Trigram plot
+from collections import defaultdict
+freq_dict = defaultdict(int)
+for sent in sar_wost_lem_df["sarcasm"]:
+    for word in generate_ngrams(sent,3):
+        freq_dict[word] += 1
+fd_sorted = pd.DataFrame(sorted(freq_dict.items(), key=lambda x: x[1])[::-1])
+fd_sorted.columns = ["word", "wordcount"]
+sar_2 = horizontal_bar_chart(fd_sorted.head(50), '#9B59B6')
+
+
+freq_dict = defaultdict(int)
+for sent in acc_wost_lem_df["acclaim"]:
+    for word in generate_ngrams(sent,3):
+        freq_dict[word] += 1
+fd_sorted = pd.DataFrame(sorted(freq_dict.items(), key=lambda x: x[1])[::-1])
+fd_sorted.columns = ["word", "wordcount"]
+acc_2 = horizontal_bar_chart(fd_sorted.head(50), '#58D68D')
+
+# Creating two subplots
+fig = tools.make_subplots(rows=1, cols=2, vertical_spacing=0.04,horizontal_spacing=0.15,
+                          subplot_titles=["Frequent Trigrams of Sarcasm Headlines", 
+                                          "Frequent Trigrams of Acclaim Headlines"])
+fig.append_trace(sar_2, 1, 1)
+fig.append_trace(acc_2, 1, 2)
+fig['layout'].update(height=1200, width=900, paper_bgcolor='rgb(233,233,233)', title="Trigram Plots Sarcasm Vs Acclaim after removing Stopwords")
+iplot(fig, filename='word-plots')
+
+
+
+
+
+from wordcloud import STOPWORDS
+import string
+## Number of words in the text ##
+sar_acc["num_words"] = sar_acc["headline"].apply(lambda x: len(str(x).split()))
+
+## Number of unique words in the text ##
+sar_acc["num_unique_words"] = sar_acc["headline"].apply(lambda x: len(set(str(x).split())))
+
+## Number of characters in the text ##
+sar_acc["num_chars"] = sar_acc["headline"].apply(lambda x: len(str(x)))
+
+## Number of stopwords in the text ##
+sar_acc["num_stopwords"] = sar_acc["headline"].apply(lambda x: len([w for w in str(x).lower().split() if w in STOPWORDS]))
+
+## Number of punctuations in the text ##
+sar_acc["num_punctuations"] =sar_acc['headline'].apply(lambda x: len([c for c in str(x) if c in string.punctuation]) )
+
+## Number of title case words in the text ##
+sar_acc["num_words_upper"] = sar_acc["headline"].apply(lambda x: len([w for w in str(x).split() if w.isupper()]))
+
+## Number of title case words in the text ##
+sar_acc["num_words_title"] = sar_acc["headline"].apply(lambda x: len([w for w in str(x).split() if w.istitle()]))
+
+## Average length of the words in the text ##
+sar_acc["mean_word_len"] = sar_acc["headline"].apply(lambda x: np.mean([len(w) for w in str(x).split()]))
+
+
+
+
+
+
+
+## Truncate some extreme values for better visuals ##
+import matplotlib.pyplot as plt
+import seaborn as sns
+color = sns.color_palette()
+
+sar_acc['num_words'].loc[sar_acc['num_words']>60] = 60 #truncation for better visuals
+sar_acc['num_punctuations'].loc[sar_acc['num_punctuations']>10] = 10 #truncation for better visuals
+sar_acc['num_chars'].loc[sar_acc['num_chars']>350] = 350 #truncation for better visuals
+
+sar_acc['num_words'].loc[sar_acc['num_words']>60] = 60 #truncation for better visuals
+sar_acc['num_punctuations'].loc[sar_acc['num_punctuations']>10] = 10 #truncation for better visuals
+sar_acc['num_chars'].loc[sar_acc['num_chars']>350] = 350 #truncation for better visuals
+
+f, axes = plt.subplots(3, 1, figsize=(10,20))
+sns.boxplot(x='is_sarcastic', y='num_words', data=sar_acc, ax=axes[0])
+axes[0].set_xlabel('is_sarcastic', fontsize=12)
+axes[0].set_title("Number of words in each class", fontsize=15)
+
+sns.boxplot(x='is_sarcastic', y='num_chars', data=sar_acc, ax=axes[1])
+axes[1].set_xlabel('is_sarcastic', fontsize=12)
+axes[1].set_title("Number of characters in each class", fontsize=15)
+
+sns.boxplot(x='is_sarcastic', y='num_punctuations', data=sar_acc, ax=axes[2])
+axes[2].set_xlabel('is_sarcastic', fontsize=12)
+#plt.ylabel('Number of punctuations in text', fontsize=12)
+axes[2].set_title("Number of punctuations in each class", fontsize=15)
+plt.show()
+
+
+
+
+
+#Getting X and Y ready
+from sklearn.preprocessing import LabelEncoder
+X = sar_acc.headline
+Y = sar_acc.is_sarcastic
+le = LabelEncoder()
+Y = le.fit_transform(Y)
+Y = Y.reshape(-1,1)
+
+
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2)
+
+
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing import sequence
+max_words = 1000
+max_len = 150
+tok = Tokenizer(num_words=max_words)
+tok.fit_on_texts(X_train)
+sequences = tok.texts_to_sequences(X_train)
+sequences_matrix = sequence.pad_sequences(sequences,maxlen=max_len)
+
+
+
+from keras.layers import LSTM, Activation, Dense, Dropout, Input, Embedding
+from keras.optimizers import RMSprop
+from keras.models import Model
+def RNN():
+    inputs = Input(name='inputs',shape=[max_len])
+    layer = Embedding(max_words,50,input_length=max_len)(inputs)
+    layer = LSTM(64)(layer)
+    layer = Dense(256,name='FC1')(layer)
+    layer = Activation('relu')(layer)
+    layer = Dropout(0.2)(layer)
+    layer = Dense(1,name='out_layer')(layer)
+    layer = Activation('sigmoid')(layer)
+    model = Model(inputs=inputs,outputs=layer)
+    return model
+
+# Compiling RNN
+
+
+model = RNN()
+model.summary()
+model.compile(loss='binary_crossentropy',optimizer=RMSprop(),metrics=['accuracy'])
+
+
+from keras.callbacks import EarlyStopping
+model.fit(sequences_matrix,Y_train,batch_size=100,epochs=5,
+          validation_split=0.1,callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)])
+
+
+test_sequences = tok.texts_to_sequences(X_test)
+test_sequences_matrix = sequence.pad_sequences(test_sequences,maxlen=max_len)
+
+
+accr = model.evaluate(test_sequences_matrix,Y_test)
+print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0],accr[1]))
 
 
 
